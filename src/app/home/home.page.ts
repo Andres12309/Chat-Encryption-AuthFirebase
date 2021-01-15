05e01a2953eedec290e08ad7b382b43d84865e5a
Andres12309 <andrius12309@gmail.com>
2021-01-15 00:32:05 -0500

    Chat-Firebase-Auth.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonContent } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { AuthService, Message } from '../servicios/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild(IonContent) content: IonContent;
 
+  messages: Observable<Message[]>;
+  newMsg= '';
+
+  constructor(private autservice:AuthService,private router:Router) {}
+  
+  ngOnInit(){
+    this.messages = this.autservice.getChatMessages();
+  }
+
+  sendMessage(){
+    this.autservice.addChatMessage(this.newMsg).then(()=>{
+      this.newMsg = '';
+      this.content.scrollToBottom();
+    })
+  }
+
+  logout(){
+    this.autservice.logout().then(()=>{
+      this.router.navigateByUrl('/',{replaceUrl:true});
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,15 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyAI-4aTFHfYu1RDL9enDIb0OsxVb6KP29w",
+  authDomain: "chat-login-cifrado.firebaseapp.com",
+  projectId: "chat-login-cifrado",
+  storageBucket: "chat-login-cifrado.appspot.com",
+  messagingSenderId: "884953821645",
+  appId: "1:884953821645:web:070193b3452f6637f7d484",
+  measurementId: "G-REYWGX5WGZ"
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
